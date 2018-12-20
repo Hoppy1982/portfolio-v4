3e7d7d73f2bfaedafecf216854613043944a9fa6
mark hopcraft <mark82hopcraft@gmail.com>
2018-12-20 14:46:43 +0000

    edited todo proj desc

--- client/src/components/projects/ToDo/ToDoProjectDesc.js
+++ client/src/components/projects/ToDo/ToDoProjectDesc.js
@@ -6,38 +6,34 @@ function ToDoProjectDesc(props) {
   return <article className={props.className}>
     <h4>Project Desciption</h4>
     <p>
-      I made this in vanilla javascript without using any libraries other
-      than React with the idea that it would be good practice at using
-      classes to organise the different particle behaviours. Originally
-      I thought I could use it as full page navigation system. I was going
-      to have the particles form the chosen nav link then all zoom off down
-      a wormhole. They'd then zoom out of wormhole on the destination
-      page and do something else like make an animated pomodoro timer.
-      In the end though I settled on leaving it as a self contained project.
+      I started this project with the objective of learning mySql and the MVC
+      pattern. Following the principal of keeping things simple there's no login
+      in / auth at the moment.
+    </p>
+    <p>
+      The glaring consequence of this is that it's a to do list for the entirity
+      of planet Earth at present where anybody can come along and edit it.
+    </p>
+    <p>
+      Future plans include adding some kind of login. I added in fields for
+      priority and completion with the intention of being able to sort and
+      filter by them. That's also still to implement. I'd also like to add in
+      custom categories later.
     </p>
 
     <h4>How It Works</h4>
     <p>
-      There are 3 particle classes, a super Particle class and then sub
-      classes for HoldPatternParticle & LettersParticle. The 2 particle
-      types live in separate arrays. To transition between behaviour types
-      they're pretty much just popped off one array and onto the other.
+      The apps got it's own express server which handles URL requests based on
+      user selections. Depending on the request type it goes through a bit of
+      validation before a query string is passed to mysql.
     </p>
     <p>
-      The hold pattern particles follow cubic bezier curves. Those curves
-      are defined by 4 coordinates, start, end & 2 control points. I used
-      an array of pre-determined waypoints and randomized a little distance
-      from those to get the start and end points. The control points are
-      pretty much randomized inside a square whose diagonals are bound by
-      the start and end points.
+      The response object
+      is used by the front end to render. The reponse is an array of ToDo / Task
+      objects. Array.map() is used in dynamically generate a react component
+      for each array element.
     </p>
-    <p>
-      For the letters particles I created an array of shape objects which
-      specificies the coordinates of each character vertex as a ratio of the
-      space assigned to each character. The space assigned to each character
-      is dependent on the longest word provided. Letter particles are given
-      target vertex to fly to.
-    </p>
+
   </article>
 }
 
