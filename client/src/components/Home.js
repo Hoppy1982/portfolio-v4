20b3750173a8d952f08ab9fb28d22bb8a4cd0e1e
mark hopcraft <mark82hopcraft@gmail.com>
2018-12-27 13:00:03 +0000

    Rewrote home page text and styled it a bit

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,37 +1,67 @@
 import React, { Component } from 'react'
 import styled from 'styled-components'
+import commonStyles from '../common-styles'
 
 
 class Home extends Component {
   render() {
     return(
-      <section className={this.props.className}>
-        <p className='homepage-site-desc'>
-          Hello and welcome to my site!
-          <br />
-          <br />
-          The motivation for building this site was to learn React,
-          as well as host my projects directly and provide links to other things I’ve built.
-          I’ve got lots of ideas, so there’s plenty more to come;
-          all will be continually added on this site as they come into fruition.
-         </p>
-      </section>
+      <div className={this.props.className}>
+        <article>
+          <p>
+            Hello and welcome to my site.
+            <br /><br />
+            The motivation for building this site was to learn React & host my
+            other projects.
+            <br /><br />
+            The site is hosted on Digital Ocean droplet. I chose that hosting solution as I
+            wanted something fairly bare metal.
+            <br /><br />
+            I’ve set up subdomains for some of my projects which then run
+            on their own small servers written in express.
+            <br /><br />
+            Nginx is set up as a reverse proxy and certbot was
+            used to implement SSL with LetsEncrypt.
+            <br /><br />
+            I used PM2 to manage the backend processes. To update
+            the web server I pull changes from a git repo and then rebuild on the server with webpack.
+            <br /><br />
+            I use
+            shell commands on the droplet to avoid needing a desktop GUI to save system resources.
+          </p>
+        </article>
+      </div>
     )
   }
 }
 
 
 const StyledHome = styled(Home)`
-  box-sizing: border-box;
-  font-family: lato;
+  ${commonStyles.defaultStyles}
+  font-size: 16px;
+  margin-top: 1.5em;
 
-  .homepage-site-desc {
-    margin-left: auto;
-    margin-right: auto;
-    text-align: center;
-    font-size: 1.4em;
-    max-width: 480px;
+  article {
+    margin: ${commonStyles.sideMarginOne};
+    border: solid lightgrey 2px;
+    border-radius: 8px;
+    box-shadow: 0px 4px 8px #575859;
+    background-color: ${commonStyles.colorOne};
+    color: #fff;
   }
+
+  p {
+    margin: ${commonStyles.sideMarginOne};
+    text-align: center;
+    font-size: calc(1.2em + 0.5vw);
+  }
+
+  @media only screen
+    and (min-device-width: 1200px)
+    and (min-width: 1200px) {
+      flex: 1 1 100%;
+      max-width: 1200px;
+    }
 `
 
 
